dddee272fdfe5f90097938effbdc5c5f5b570057
sinanurun <sinan.urun@deu.edu.tr>
2018-01-18 21:10:18 +0300

    Ders 05
    değişken kavramı

---
+++ ders 05/degisken degisen.py
@@ -0,0 +1,7 @@
+kullanici_adi = "Mehmet"
+print("hoş geldin",kullanici_adi)
+kek = "Tarçınlı kek"
+print("yiyeceğiniz",kek)
+icecek = "limonata"
+print("içeceğiniz",icecek)
+print("afiyet olsun")
---
+++ ders 05/yenitanisma.py
@@ -0,0 +1,9 @@
+print("merhaba benim adım Mahmut")
+print("senin adın nedir ?")
+isim = input()
+print(isim)
+print("tanıştığıma memnun oldum",isim)
+print("ben izmirliyim sen nerelisin ?")
+sehir = input()
+print("senin olduğun",sehir,"şehri de çok güzel")
+
