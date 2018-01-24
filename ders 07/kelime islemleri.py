4b20d470fa9175a109c0ec124397450150f01374
sinanurun <sinan.urun@deu.edu.tr>
2018-01-24 10:57:55 +0300

    ders 07
    artmetiksel işlemler

---
+++ ders 07/kelime islemleri.py
@@ -0,0 +1,8 @@
+kelime1 = "mahmut"
+print(kelime1)
+kelime2 = "mehmet"
+yeni_kelime = kelime1 + " " +kelime2
+print(yeni_kelime)
+print(kelime1*5)
+print(yeni_kelime*2)
+print(yeni_kelime / 2)
---
+++ ders 07/temelaritmetik.py
@@ -0,0 +1,23 @@
+print(25+45)
+print(21+3)
+sayi1 = 4
+sayi2 = 5
+topla = sayi1 + sayi2
+print(topla)
+# yukarıda ki kodlar ile birlikte toplama işlemi gerçekleştirilmiştir
+print(47-21)
+print(21-3)
+fark = sayi2 - sayi1
+print(fark)
+# yukarıda ki kodlar ile birlikte çıkartma işlemi yapılmıştır
+print(47*62)
+print(8*74)
+carp = sayi1 * sayi2
+print(carp)
+# yukarıda ki kodlar ile birlikte çarpma işlemi yapılmıştır
+print(4/8)
+print(100/20)
+bol1 = sayi2/sayi1
+bol2 = int(sayi2/sayi1)
+print(bol1)
+print(bol2)
